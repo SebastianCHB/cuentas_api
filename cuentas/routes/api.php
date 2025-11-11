37a888c1c75cc31890f3ca23f3125e7ef3bfd668
Sebastian Chacon B <87223451+SebastianCHB@users.noreply.github.com>
2025-11-11 14:36:54 -0700

    ...

--- cuentas/routes/api.php
+++ cuentas/routes/api.php
@@ -7,16 +7,14 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\TransactionsController;
 //EHDPOINT
-Route::resource('accounts', AccountsController::class);
-Route::resource('categories', CategoriesController::class);
-Route::resource('transactions', TransactionsController::class);
-
-Route::post('changestatus', [AccountsController::class, 'changeStatus']);
 
 Route::post('/login',[AuthController::class, 'login']);
 
+Route::middleware("auth:api")->group(function(){
+Route::resource('accounts', AccountsController::class);
+Route::resource('categories', CategoriesController::class);
+Route::resource('transactions', TransactionsController::class);
+Route::post('changestatus', [AccountsController::class, 'changeStatus']);
+});
 
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
